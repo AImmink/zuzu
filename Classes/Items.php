6aadeb25ad748815fefd4816c6e3ec4e940cdb78
Alex <70693758+AImmink@users.noreply.github.com>
2022-01-18 10:47:58 +0100

    Add files via upload

---
+++ Classes/Items.php
@@ -0,0 +1,18 @@
+<?php
+
+class Items
+{
+    public $id;
+    public $name;
+    public $image;
+    public $description;
+    
+    public function __construct()
+    {
+        settype($this->id, 'integer');
+        settype($this->name, 'string');
+        settype($this->image, 'string');
+        settype($this->description, 'string');
+    }
+}
+
